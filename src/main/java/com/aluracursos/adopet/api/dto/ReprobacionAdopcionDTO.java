05b16702a9746fe460f9817862d71232d44cbbf0
PaulRodriguezAsihama <pau.rodriguez.asihama@hotmail.com>
2024-11-17 18:58:41 -0400

    Hasta la 5ta leccion con todo funcionando

---
+++ src/main/java/com/aluracursos/adopet/api/dto/ReprobacionAdopcionDTO.java
@@ -0,0 +1,9 @@
+package com.aluracursos.adopet.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ReprobacionAdopcionDTO(
+        @NotNull Long idAdopcion,
+        @NotBlank String justificacion) {
+}
